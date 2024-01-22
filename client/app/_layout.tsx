9b15b4582952f5f9bed2881806df6c1edf66b3a8
Rodion Hudkov <hudkov@happyend.cz>
2024-01-22 13:58:24 +0100

    Add TopBar and BottomBar components

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,9 +1,16 @@
 import { Stack } from "expo-router"
+import { SafeAreaView } from "react-native-safe-area-context";
+import { View } from "react-native";
+import { BottomBar, TopBar } from "../components";
 
 
 const Layout = () => {
   return (
-    <Stack />
+    <View style={{flex: 1}}>
+      <TopBar title="Home" />
+      <Stack />
+      <BottomBar />
+    </View>
   )
 }
 
--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,12 +1,22 @@
-import { Text, View } from "react-native"
+import { AppRegistry, Text, View } from "react-native"
+import { PaperProvider, MD3LightTheme as DefaultTheme } from "react-native-paper";
 
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    secondary: 'yellow',
+  }
+}
 
-const App = () => {
+export default function App() {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <PaperProvider theme={theme}>
+      <Text style={{color: theme.colors.primary}}>App</Text>
+    </PaperProvider>
   );
 }
 
-export default App;
+AppRegistry.registerComponent('App', () => App);
+// export default App;
